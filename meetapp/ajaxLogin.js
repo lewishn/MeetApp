b9308bc2a72ed0c1ce808e23f980fb25989294a7
Lewis <lewis.haris@gmail.com>
2015-04-17 14:12:47 +0800

    Added google login button

--- meetapp/ajaxLogin.js
+++ meetapp/ajaxLogin.js
@@ -38,3 +38,45 @@ $(document).ready(function()
 		return false;
 	});
 });
+
+
+
+// Note, these have to be outside here or it won't work.
+(function() {
+	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+	po.src = 'https://apis.google.com/js/client:plusone.js?onload=signinCallback';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
+
+function signinCallback(authResult) {
+	if (authResult['status']['signed_in']) {
+	    doGoogleLogin();
+	} else {
+	    console.log('Sign-in state: ' + authResult['error']);
+	}
+}
+
+function doGoogleLogin() {
+	var user_id, user_name, user_image_url;
+
+	gapi.client.load('plus', 'v1', function() {
+		  var request = gapi.client.plus.people.get({
+		    'userId': 'me'
+		  });
+		  request.execute(function (resp){
+		    console.log(resp);
+		    if(resp.id){
+		      user_id = resp.id;
+		    }
+		    if(resp.displayName){
+		      user_name = resp.displayName;
+		    }
+		    if(resp.image && resp.image.url){
+		      user_image_url = resp.image.url;
+		    }
+		});
+	});
+
+	// do ajax whatever here.
+	
+}
